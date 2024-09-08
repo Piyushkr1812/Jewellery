cbbc5a17e179439f1aaa8aa393f5054b48ae192b
Piyushkr1812 <92864991+Piyushkr1812@users.noreply.github.com>
2024-09-08 10:54:04 +0530

    Update urls.py

--- Project_django6/urls.py
+++ Project_django6/urls.py
@@ -20,12 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
     path('para/', include('para.urls')),  # Assuming you have an app named 'para'
     path('super/', include('super.urls')),  # Assuming you have an app named 'super'
     path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
